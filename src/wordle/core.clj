63f31e650eb8dbca62ff459eb18a92bc1f071866
John Midgley <john.midgley@gmail.com>
2022-02-01 14:01:39 -0800

    Got path parsing / apply working

--- src/wordle/core.clj
+++ src/wordle/core.clj
@@ -14,11 +14,9 @@
          (map str/lower-case)
          (reduce conj #{}))))
 
-
 (defn parse-line [line]
   (let [[word freq] (str/split line #",")]
     [(str/lower-case word) (read-string freq)]))
-
 
 (defn read-freqs [filename]
   (with-open [rdr (clojure.java.io/reader filename)]
@@ -26,32 +24,27 @@
          (mapcat  parse-line) 
          (apply hash-map))))
 
-
-(defn match [words pred]
+(defn match-pred [words pred]
   (->> words
        (filter #(pred %))
        (reduce conj #{})))
 
+(defn match-char* 
+  ([words char]
+   (match-char* words char nil))
+  ([words char pos]
+   (let [pred (if pos
+                #(= char (get % pos)) 
+                #(str/includes? % (str char)))]
+     (match-pred words pred))))
 
-(defn match-at-pos* [words char pos]
-  (match words #(= char (get % pos))))
-
-
-(def match-at-pos (memoize match-at-pos*))
-
-
-(defn match-any-pos* [words char]
-  (match words #(str/includes? % (str char))))
-
-
-(def match-any-pos (memoize match-any-pos*))
+(def match-char (memoize match-char*))
 
 
 (defn match-any-letter [words word]
   (->> (seq word)
-       (map #(match-any-pos words %))
+       (map #(match-char words %))
        (reduce set/union)))
-
 
 (defn weight-by-uniform [words]
   (count words))
@@ -71,8 +64,28 @@
          (:word))))
 
 
+(defn op-fn [op]
+  (case op 
+    \+ set/intersection
+    \- set/difference))
+
+
+(defn apply-segment [words [op char pos]]
+  (let [f (op-fn op)
+        matches (match-char words char pos)]    
+    (f words matches)))
+
+
 (defn parse-path [path]
-  (re-seq #"[+-]\w\d?" path))
+  (map (fn [[op char pos]] [op char (if pos (read-string (str pos)))])
+       (re-seq #"[+-]\w\d?" path)))
+
+
+(defn apply-path [words path]
+  (let [segments (parse-path path)]
+    (reduce apply-segment words segments)))
+
+
 
 (defonce freqs (read-freqs (str (System/getProperty "user.dir") "/datasets/unigram-freq.csv")))
 (defonce all-words (read-words (str (System/getProperty "user.dir") "/datasets/scrabble-twl.txt")))
