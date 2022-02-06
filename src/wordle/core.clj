ba5e96b70ee945db088158ac9d50625653abe721
John Midgley <john.midgley@gmail.com>
2022-02-05 16:36:37 -0800

    Work on better check guess

--- src/wordle/core.clj
+++ src/wordle/core.clj
@@ -69,6 +69,7 @@
          (:word))))
 
 
+
 (defn apply-segment [words [op char pos]]
   (let [op-fn (case op 
                 \+ set/intersection
@@ -86,6 +87,7 @@
   (let [segments (parse-path path)]
     (reduce apply-segment words segments)))
 
+
 ; Still not perfect - could be more constraining by taking into account previously matched letters
 ; e.g. state as a guess - the second t could produce -t2-t3-t4, since there's only one t, but 
 ; currenlty it just produces +t-t3
@@ -99,15 +101,39 @@
         (apply str))))
 
 
+(defn check-guess-letter [matched-target-letters       ; #{letter*}
+                          free-target-letter-positions ; [pos letter]*
+                          remaining-letter-guesses     ; [pos letter]*
+                          [pos letter]]                ; letter guess [pos letter]
+  (let [letter-in-matched-target (mtls (second lg))
+        l-in-ftls (contains? (mapv second ftls) (second lg))]
+    (if-let [target-letter-pos (first (filter #(= letter (second %)) free-target-letter-positions))]
+      (concat [[\- letter pos]] (if (matched-target-letters letter) [[\+ letter]]))
+      (concat [[\- letter  (if (matched-target-letters letter) pos)]
+               nil]))))
+
+; (check-guess1 "happy" "daspd")
+; (check-guess-letter #{\a \p} [[0 \h] [2 \p] [4 \y]] [[2 \s] [4 \d]] [0 \d]) 
+
+
+; letter in | letter in | l = letter guess
+; free      | matched   | p = position of guess
+; target ls | target ls | x = all remaining letter guess positions
+; ----------+-----------+------------------------------------------------------
+; false     | false     | -l = -lp-lx*
+; false     | true      | -lp-lx* 
+; true      | false     | -lp+|lx* 
+; true      | true      | -lp+|lx*
+
+
 (defn check-guess1 [target guess]
-  (let [{same true diff false} (->> (map vector (range) target guess)
-                                    (group-by (fn [[p t g]] (= t g))))
-        pl (fn [c] (map (fn [[p _ l]] [p l]) c))
-        state {:same (pl same)
-               :diff (pl diff)
-               :free (map second diff)}]
-    state
-    ))
+  (let [{same true diff false} (->> (map vector (range) target guess)                                  
+                                    (group-by (fn [[p t g]] (= t g))))]  ; p=pos, (t=target, g=guess) letters
+    (loop [ut same
+           ft diff
+           gl (map (fn [[p _ l]] [p l])  diff)] ; guesses-left
+      (if (empty? gl) ut
+          (recur ut ft (rest gl))))))
 
 
 
@@ -133,3 +159,4 @@
 (defn play-by-mode [target]
   (play words (partial guess-by-mode freqs) target))
        
+; (play-by-mode "pleat")
